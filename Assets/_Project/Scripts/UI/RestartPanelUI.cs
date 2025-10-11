2a2d6a36f1b636aa3e55eb192465a35bc1b22055
pangolier33 <gled601@yandex.ru>
2025-10-11 19:30:33 +0400

    changes

--- Assets/_Project/Scripts/UI/RestartPanelUI.cs
+++ Assets/_Project/Scripts/UI/RestartPanelUI.cs
@@ -6,7 +6,6 @@ namespace _Project.Scripts.UI
     public class RestartPanelUI : MonoBehaviour
     {
         [SerializeField] private TMP_Text _scoreText;
-        private int _score;
 
         public void SetScore(int score)
         {
