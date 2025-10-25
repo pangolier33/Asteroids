324693cfbf8c7a7458a26b45b2e19fc382b552e1
pangolier33 <gled601@yandex.ru>
2025-10-26 02:12:22 +0400

    changes

--- Assets/_Project/Scripts/Creatures/Enemy/UFOMovement.cs
+++ Assets/_Project/Scripts/Creatures/Enemy/UFOMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace _Project.Scripts.Creatures.Enemy
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class UfoMovement : MonoBehaviour
     {
         [SerializeField] private float _moveSpeed = 2f;
